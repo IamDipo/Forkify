883b2850cb258cea00031c1712c61b96bc0889dd
Fatogun Oladipupo <fatogunoladipupo@gmail.com>
2025-10-01 13:21:57 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '29f57308-7b88-4177-a689-67a275055069';
+export const MODAL_CLOSE_SEC = 2.5;
